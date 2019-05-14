c1df4022b305440e44db0de9f94caed5001afd57
leoking01 <leoking02@foxmail.com>
2019-05-14 08:19:52 +0800

    InitializeComponent

---
+++ VisionControl/VisionControl.AppWizard.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace VisionControl
+{
+  partial class VisionControl
+  {
+    ///////////////////////// START WIZARD GENERATED
+    // cognex.wizard.globals.begin
+    private const string mVppFilename = @"../../../QuickBuild2Jobs/QuickBuild2Jobs.vpp";
+    private const string mApplicationName = @"Application Name";
+    private bool mUsePasswords = true;
+    private static bool mQuickBuildAccess = true;
+    private const string mDefaultAdministratorPassword = "admin";
+    private const string mDefaultSupervisorPassword = "super";
+    private static System.DateTime mGenerationDateTime = new System.DateTime(2006, 8, 14, 15, 27, 40);
+    private static string mGeneratedByVersion = "25.0.0.0";
+    // cognex.wizard.globals.end
+    ///////////////////////// END WIZARD GENERATED
+
+    private void Wizard_FormLoad()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.formloadactions
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_AttachPropertyProviders()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.attachpropertyproviders
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_DetachPropertyProviders()
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.detachpropertyproviders
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_EnableControls(bool running)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.enablecontrols
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_AddJobTabs(System.Collections.ArrayList newPagesList)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.addjobtabs
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+    private void Wizard_UpdateJobResults(int idx, Cognex.VisionPro.ICogRecord result)
+    {
+      ///////////////////////// START WIZARD GENERATED
+      // cognex.wizard.updatejobresults
+      ///////////////////////// END WIZARD GENERATED
+    }
+
+  }
+}
